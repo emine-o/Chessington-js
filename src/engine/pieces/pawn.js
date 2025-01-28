99696bf580cb8660b2a561f9f2a8b2f86ad10859
Preeti Mishra <preeti.mishra@softwire.com>
2025-01-28 14:52:46 +0000

    Coded for pawn Red-2

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -6,13 +6,30 @@ export default class Pawn extends Piece {
     constructor(player) {
         super(player);
     }
-
+    
     getAvailableMoves(board) {
         let location = board.findPiece(this)
+               
         if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
-        } else {
-            return Square.at(location.row - 1, location.col)
-        }
+            if(location.row === 1)
+            {
+                return [Square.at(location.row + 1, location.col),Square.at(location.row + 2, location.col)]
+            }
+            else
+            {
+                return [Square.at(location.row + 1, location.col)]
+            }            
+        } 
+        else {
+            if(location.row === 6)
+            {
+                return [Square.at(location.row - 1, location.col),Square.at(location.row - 2, location.col)]
+            }
+            else
+            {
+                return [Square.at(location.row - 1, location.col)]
+            }           
+        }       
+
     }
 }
