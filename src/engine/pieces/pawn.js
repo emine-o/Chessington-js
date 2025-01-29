2eee2c30a22d8995822a5b36e6398f5fcb9e903f
Preeti Mishra <preeti.mishra@softwire.com>
2025-01-29 16:07:09 +0000

    Rough code for diagnol case Red-18

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -9,25 +9,32 @@ export default class Pawn extends Piece {
     
     getAvailableMoves(board) {
         let location = board.findPiece(this)
-        
+        let arrayAvailableMoves = []
         
         if (this.player === Player.WHITE) {
+            
             if(location.row === 1)
-            {
+            {                
                 if(!(board.getPiece(Square.at(location.row + 1, location.col))) && 
                 !(board.getPiece(Square.at(location.row + 2, location.col))))
                 {
                     return [Square.at(location.row + 1, location.col),Square.at(location.row + 2, location.col)]
+                    // arrayAvailableMoves.push(Square.at(location.row + 1, location.col),Square.at(location.row + 2, location.col))
+                    // return arrayAvailableMoves;
                 }
                 else if(!(board.getPiece(Square.at(location.row + 1, location.col))) && 
                 typeof(board.getPiece(Square.at(location.row + 2, location.col))) === Square)
                 {
                     return [Square.at(location.row + 1, location.col)]
+                    // arrayAvailableMoves.push(Square.at(location.row + 1, location.col))
+                    // return arrayAvailableMoves;
                 }  
                 else if (!(board.getPiece(Square.at(location.row + 2, location.col))) && 
                 typeof(board.getPiece(Square.at(location.row + 1, location.col))) === Square)
                 {
                     return [Square.at(location.row + 2, location.col)]
+                    // arrayAvailableMoves.push(Square.at(location.row + 2, location.col))
+                    // return arrayAvailableMoves;
                 }                
                 else
                 {
@@ -36,15 +43,30 @@ export default class Pawn extends Piece {
             }
             else
             {
-                if(!(board.getPiece(Square.at(location.row + 1, location.col))))
+                // if((typeof board.getPiece(Square.at(location.row + 1, location.col-1)) !== null ||
+                // typeof board.getPiece(Square.at(location.row + 1, location.col-1)) !== undefined) &&
+                // location.row !==7)
+                // {
+                //     //board.Square.at(location.row + 1, location.col-1).player                
+                //      return [Square.at(location.row + 1, location.col-1)]
+                //     //arrayAvailableMoves.push(Square.at(location.row + 1, location.col-1))
+                //     //return arrayAvailableMoves;
+                // }        
+                if (location.row === 7)
+                {
+                    return []
+                }
+                else if(!(board.getPiece(Square.at(location.row + 1, location.col))))
                 {
                     return [Square.at(location.row + 1, location.col)]
+                    //arrayAvailableMoves.push(Square.at(location.row + 1, location.col))
+                    // return arrayAvailableMoves;
                 }
                 else
                 {
                     return [];
                 }
-            }            
+            }                
         } 
         else {
             if(location.row === 6)
@@ -53,16 +75,22 @@ export default class Pawn extends Piece {
                     !(board.getPiece(Square.at(location.row - 2, location.col))))
                 {
                     return [Square.at(location.row - 1, location.col),Square.at(location.row - 2, location.col)]
+                    // arrayAvailableMoves.push(Square.at(location.row - 1, location.col),Square.at(location.row - 2, location.col))
+                    // return arrayAvailableMoves;
                 }
                 else if(!(board.getPiece(Square.at(location.row - 1, location.col))) && 
                     typeof(board.getPiece(Square.at(location.row - 2, location.col))) === Square)
                 {
                     return [Square.at(location.row - 1, location.col)]
+                    // arrayAvailableMoves.push(Square.at(location.row - 1, location.col))
+                    // return arrayAvailableMoves;
                 }  
                 else if (!(board.getPiece(Square.at(location.row - 2, location.col))) && 
                     typeof(board.getPiece(Square.at(location.row - 1, location.col))) === Square)
                 {
                     return [Square.at(location.row - 2, location.col)]
+                    // arrayAvailableMoves.push(Square.at(location.row - 2, location.col))
+                    // return arrayAvailableMoves;
                 }
                 else
                 {
@@ -71,9 +99,15 @@ export default class Pawn extends Piece {
             }
             else
             {
-                if(!(board.getPiece(Square.at(location.row - 1, location.col))))
+                if (location.row === 0)
+                {
+                    return []
+                }
+                else if(!(board.getPiece(Square.at(location.row - 1, location.col))))
                 {
                     return [Square.at(location.row - 1, location.col)]
+                    // arrayAvailableMoves.push(Square.at(location.row - 1, location.col))
+                    // return arrayAvailableMoves;
                 }
                 else
                 {
